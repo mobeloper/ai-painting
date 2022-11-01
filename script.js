f4f17de1cb2695991cfd183f0c3b0fc03ca8d1e3
Nico <nicolas38.jeanne@hotmail.fr>
2022-11-01 17:05:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+console.log("Hey! Welcome to MyAIPainting.com console.\n\nIf you're here, I guess you are a dev! Not much to do here honestly.\n\nIf you find a bug or have feedback, here is my twitter: @nico_jeannen");
+
+
+const cta = document.querySelectorAll(".main-cta");
+
+cta.forEach((cta) => { 
+    cta.addEventListener("click", (e) => {
+        window.location.href = 'https://buy.stripe.com/4gw9Cve79aJldPi8ww'
+    });
+})
